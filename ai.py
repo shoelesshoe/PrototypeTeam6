d4615c4016f0dd120871b01edbed8cc698fe7fe9
shoe <70523543+shoelesshoe@users.noreply.github.com>
2021-06-04 21:32:15 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,4 +1,3 @@
-
 from nltk.corpus import stopwords
 from nltk.cluster.util import cosine_distance
 import numpy as np
@@ -23,13 +22,15 @@ def sentence_similarity(ls, ls1, stopwords):
         if w in stopwords:
             continue
         vector2[all_words.index(w)] += 1
+    allwords = list(ls + ls1)
+    
     return (np.dot(vector1, vector2) / (sqrt(np.dot(vector1, vector1)) * sqrt(np.dot(vector2,vector2))))
 
 def generate_summary(file_name, top_n):
     stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
     summarize_text = []
 
-    file = open(file_name, "r")
+    file = open(file_name, "r",encoding="utf8")
     filedata = file.readlines()
     
     article = filedata[0].split(". ")
@@ -38,7 +39,22 @@ def generate_summary(file_name, top_n):
     for sentence in article:
         #print(sentence)
         sentences.append(sentence.split(" "))
-
+    
+    word_frequencies = {}
+   # print(sentences)
+    for s in sentences:
+        for word in s:
+            if word not in stop_words:
+                if word not in word_frequencies.keys():
+                    word_frequencies[word] = 1
+                else:
+                    word_frequencies[word] += 1
+    sortlst = []
+    for word in word_frequencies.keys():
+        if word_frequencies[word]>=5:
+            sortlst.append(word)
+    print(sortlst)
+    
     sentence_similarity_martix = np.zeros((len(sentences), len(sentences)))
     for i in range(len(sentences)):
         for j in range(len(sentences)):
